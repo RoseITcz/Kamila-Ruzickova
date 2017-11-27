5900ba55346d716cf26d7c4f2131eb34bea96157
RoseITcz <RoseITcz@users.noreply.github.com>
2017-11-27 17:40:26 +0100

    update

---
+++ fn.js
@@ -0,0 +1,21 @@
+function renderHome() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/home.html" ></object>';
+}
+function renderAbout() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/about.html" ></object>';
+}
+function renderCV() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/cv.html" ></object>';
+}
+function renderPublications() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/publications.html" ></object>';
+}
+function renderArticles() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/articles.html" ></object>';
+}
+function renderActivities() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/activities.html" ></object>';
+}
+function renderNavigation() {
+     document.getElementById("here").innerHTML='<object type="text/html" data="pages/navigation.html" ></object>';
+}
