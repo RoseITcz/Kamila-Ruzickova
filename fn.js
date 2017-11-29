45042448fb9d292a0618281508010f5d325634e5
RoseITcz <RoseITcz@users.noreply.github.com>
2017-11-29 14:00:46 +0100

    update 3

--- fn.js
+++ fn.js
@@ -1,18 +1,34 @@
+var prevLink="home";
+function changeLinkStyle(eleID){
+    document.getElementById(eleID).style.background='#61dafb';
+    document.getElementById(eleID).style.color='black';
+    document.getElementById(prevLink).style.background='#022731';
+    document.getElementById(prevLink).style.color='white';
+    //alert(eleID);
+    //alert(prevLink);
+    if (eleID != prevLink){
+      prevLink=eleID;
+    }
+}
+
+//Rendering MENU
 function renderHome() {
      document.getElementById("here").innerHTML='<object type="text/html" data="pages/home.html" ></object>';
+     changeLinkStyle("home");
 }
 function renderAbout() {
      document.getElementById("here").innerHTML='<object type="text/html" data="pages/about.html" ></object>';
+     changeLinkStyle("about");
 }
 function renderCV() {
      document.getElementById("here").innerHTML='<object type="text/html" data="pages/cv.html" ></object>';
+     changeLinkStyle("cv");
 }
 function renderPublications() {
      document.getElementById("here").innerHTML='<object type="text/html" data="pages/publications.html" ></object>';
-}
-function renderActivities() {
-     document.getElementById("here").innerHTML='<object type="text/html" data="pages/activities.html" ></object>';
+     changeLinkStyle("publications");
 }
 function renderNavigation() {
      document.getElementById("here").innerHTML='<object type="text/html" data="pages/navigation.html" ></object>';
+     changeLinkStyle("navigation");
 }
